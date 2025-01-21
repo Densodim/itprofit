76f1d526ef34c7c8c1e021ba8da3898f18a4d20b
Densodim <25denso@gmail.com>
2025-01-21 22:19:39 +0300

    add project

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    devServer: {
+        static: './dist',
+        open: true,
+        port: 3000,
+    },
+    mode: 'development',
+};
